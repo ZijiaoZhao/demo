8b33d71dd754f01af60c17a7250e314c49cee158
Zijiao Zhao <zijiao.zhao@inspiry.cn>
2016-12-21 10:53:13 +0800

    springboot mybatis demo1
    Signed-off-by: Zijiao Zhao <zijiao.zhao@inspiry.cn>

---
+++ springboot-mybatis-demo1/src/main/java/com/example/LamdaDemo.java
@@ -0,0 +1,20 @@
+package com.example;
+
+/**
+ * Created by zijiao on 12/15/16.
+ */
+public class LamdaDemo {
+    public static void main(String[] args) {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("run 1.......");
+            }
+        };
+
+        Runnable r1 = () -> System.out.println("run 2......");
+
+        runnable.run();
+        r1.run();
+    }
+}
